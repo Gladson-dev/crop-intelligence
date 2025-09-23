a7f954cdc9ce0cd639203ad816408dc83210150b
Monish291 <monishraju523@gmail.com>
2025-09-23 12:07:18 +0530

    chore: update frontend configuration and add .env.example

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import { fileURLToPath, URL } from 'url';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -11,7 +12,7 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
     resolve: {
       alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
+        '@': path.resolve(__dirname, './src'),
       }
     },
     server: {
